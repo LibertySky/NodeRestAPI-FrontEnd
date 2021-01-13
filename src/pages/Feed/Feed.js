e710d1c12f2fd6376204e799428fcb92e6b80cac
LibertySky <hello@LibertySkygraphics.com>
2021-01-13 10:29:18 -0400

    socket.io for real time posts status update

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import openSocket from 'socket.io-client';
 
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
@@ -39,7 +40,49 @@ class Feed extends Component {
 			.catch(this.catchError);
 
 		this.loadPosts();
+
+		const socket = openSocket('http://localhost:3021');
+		socket.on('posts', (data) => {
+			if (data.action === 'create') {
+				this.addPost(data.post);
+			} else if (data.action === 'update') {
+				this.updatePost(data.post);
+			} else if (data.action === 'delete') {
+				this.loadPosts();
+			}
+		});
 	}
+
+	addPost = (post) => {
+		this.setState((prevState) => {
+			const updatedPosts = [...prevState.posts];
+			if (prevState.postPage === 1) {
+				if (prevState.posts.length >= 2) {
+					updatedPosts.pop();
+				}
+				updatedPosts.unshift(post);
+			}
+			return {
+				posts: updatedPosts,
+				totalPosts: prevState.totalPosts + 1,
+			};
+		});
+	};
+
+	updatePost = (post) => {
+		this.setState((prevState) => {
+			const updatedPosts = [...prevState.posts];
+			const updatedPostIndex = updatedPosts.findIndex(
+				(p) => p._id === post._id
+			);
+			if (updatedPostIndex > -1) {
+				updatedPosts[updatedPostIndex] = post;
+			}
+			return {
+				posts: updatedPosts,
+			};
+		});
+	};
 
 	loadPosts = (direction) => {
 		if (direction) {
@@ -159,17 +202,17 @@ class Feed extends Component {
 					createdAt: resData.post.createdAt,
 				};
 				this.setState((prevState) => {
-					let updatedPosts = [...prevState.posts];
-					if (prevState.editPost) {
-						const postIndex = prevState.posts.findIndex(
-							(p) => p._id === prevState.editPost._id
-						);
-						updatedPosts[postIndex] = post;
-					} else if (prevState.posts.length < 2) {
-						updatedPosts = prevState.posts.concat(post);
-					}
+					// let updatedPosts = [...prevState.posts];
+					// if (prevState.editPost) {
+					// 	const postIndex = prevState.posts.findIndex(
+					// 		(p) => p._id === prevState.editPost._id
+					// 	);
+					// 	updatedPosts[postIndex] = post;
+					// }
+					// } else if (prevState.posts.length < 2) {
+					// 	updatedPosts = prevState.posts.concat(post);
+					// }
 					return {
-						posts: updatedPosts,
 						isEditing: false,
 						editPost: null,
 						editLoading: false,
@@ -206,11 +249,12 @@ class Feed extends Component {
 				return res.json();
 			})
 			.then((resData) => {
-				console.log(resData);
-				this.setState((prevState) => {
-					const updatedPosts = prevState.posts.filter((p) => p._id !== postId);
-					return { posts: updatedPosts, postsLoading: false };
-				});
+				// console.log(resData);
+				this.loadPosts();
+				// this.setState((prevState) => {
+				// 	const updatedPosts = prevState.posts.filter((p) => p._id !== postId);
+				// 	return { posts: updatedPosts, postsLoading: false };
+				// });
 			})
 			.catch((err) => {
 				console.log(err);
@@ -270,8 +314,7 @@ class Feed extends Component {
 							onPrevious={this.loadPosts.bind(this, 'previous')}
 							onNext={this.loadPosts.bind(this, 'next')}
 							lastPage={Math.ceil(this.state.totalPosts / 2)}
-							currentPage={this.state.postPage}
-						>
+							currentPage={this.state.postPage}>
 							{this.state.posts.map((post) => (
 								<Post
 									key={post._id}
